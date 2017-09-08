d6fc3f0f05ead364d5e4e809fde0a6d2a97831bb
Marc Remolt <marc.remolt@dcs-fuerth.de>
2017-09-08 16:47:28 +0200

    refactor(products): Remove unneeded function params

--- src/app/products/backend/products.reducer.ts
+++ src/app/products/backend/products.reducer.ts
@@ -15,13 +15,9 @@ import { PRODUCTS_COLLECTION_KEY } from './products.constants';
 import { actionNames } from './products.actions';
 
 export const productsReducer: ISubStateReducer<INormalizedCollectionState> = normalizedCollectionReducerFactory(
-  PRODUCTS_COLLECTION_KEY,
-  undefined,
-  {}
+  PRODUCTS_COLLECTION_KEY
 );
 
 export const currentProductReducer: ISubStateReducer<INormalizedEntityState> = normalizedEntityReducerFactory(
-  PRODUCTS_COLLECTION_KEY,
-  undefined,
-  {}
+  PRODUCTS_COLLECTION_KEY
 );
